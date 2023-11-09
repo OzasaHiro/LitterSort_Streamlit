4dfaa1d08aa6e6e1faa3f8e536d8544fcfface3e
Hiroaki Ozasa <37623652+OzasaHiro@users.noreply.github.com>
2023-11-10 04:26:11 +0900

    Update LitterSortGPT.py

--- LitterSortGPT.py
+++ LitterSortGPT.py
@@ -164,7 +164,7 @@ def main():
                             {
                                 "type": "image_url",
                                 "image_url": {
-                                    "url": f"data:image/jpeg;base64,{base64_image}" },
+                                    "url": f"https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg" },
                             },
                         ],
                     }
